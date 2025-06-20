9ece5ae73b8954b4555839a2df718e774051607e
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-06-20 13:56:06 +0530

    Change

---
+++ Complete_Problems/Daily/L00000_HelloWorld.java
@@ -0,0 +1,11 @@
+package Complete_Problems.Daily;
+
+class L00000_HelloWorld {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello World! Jayabalajee");
+        
+    }
+}
+// Hello World! Jayabalajee
